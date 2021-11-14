eeaf77d3c0e90ec35a796d405fa7452edc7253fc
EGOIST <0x142857@gmail.com>
2021-11-14 19:52:47 +0800

    chore: tweaks

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,9 @@ class PrettyError extends Error {
   constructor(message: string) {
     super(message)
     this.name = this.constructor.name
+    // @ts-expect-error
     if (typeof Error.captureStackTrace === 'function') {
+      // @ts-expect-error
       Error.captureStackTrace(this, this.constructor)
     } else {
       this.stack = new Error(message).stack
